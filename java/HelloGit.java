5c43642682cbef9a9919165ecb280a612d536c35
U-NORTH_AMERICA\shameed <shameed@na1000-shameed.na.ds.monsanto.com>
2010-07-09 14:38:00 -0500

    first revision

---
+++ java/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: shameed
+ * Date: Jul 9, 2010
+ * Time: 2:37:29 PM
+ */
+public class HelloGit {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Git!");
+  }
+}
