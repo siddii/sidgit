85f6a0470ad2abe2dd6d434603f57ac5fb66fa29
U-NORTH_AMERICA\shameed <shameed@na1000-shameed.na.ds.monsanto.com>
2010-07-20 12:24:35 -0500

    adding catch blockl

--- java/StackOverflowLimitTest.java
+++ java/StackOverflowLimitTest.java
@@ -1,16 +1,13 @@
-/**
- * Created by IntelliJ IDEA.
- * User: shameed
- * Date: Jul 9, 2010
- * Time: 2:41:18 PM
- */
 public class StackOverflowLimitTest {
 
   private static int recursionLimit = 0;
 
   public static void main(String[] args) {
-    System.out.println("recursionLimit++ = " + recursionLimit++);
-    main(args);
+    try {
+      recursionLimit++;
+      main(args);
+    } catch (StackOverflowError e) {
+      System.out.println("recursionLimit = " + recursionLimit);
+    }
   }
-  
 }
